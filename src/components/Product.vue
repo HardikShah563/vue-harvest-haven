a142e2ef7ff0b8d19954ad765202a5b78b813699
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2023-11-27 17:33:23 +0530

    cart page works

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -10,18 +10,58 @@ const prod_img = "../assets/images/store_img/bananas.png";
 export default {
     name: "Product",
     props: {
+        prodId: Number,
         prodName: String,
         prodImage: String,
         prodQty: String,
         prodPrice: Number,
         prodStockQty: Number,
+        count: Number
     },
     data() {
         return {
-            img_url: "data:image/png;base64," + this.prodImage
-        }
-    }
-}
+            count: this.count || 0,
+            prodStock: this.prodStockQty,
+            img_url: "data:image/png;base64," + this.prodImage,
+            operationData: {
+                op: "",
+                p_id: this.prodId,
+            },
+        };
+    },
+    methods: {
+        async addToCart() {
+            this.count < this.prodStock && this.count++;
+            this.operationData.op = "plus";
+            await fetch("http://127.0.0.1:5000/shop", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(this.operationData),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data.message);
+                });
+        },
+        async removeFromCart() {
+            this.count > 0 && this.count--;
+            this.operationData.op = "minus";
+            await fetch("http://127.0.0.1:5000/shop", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(this.operationData),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data.message);
+                });
+        },
+    },
+};
 </script>
 
 <template>
@@ -32,25 +72,31 @@ export default {
             </div>
 
             <div class="store-item-text">
-                <p class="store-item-title">
-                    {{ prodName }} ({{ prodQty }})
-                </p>
-                <p class="store-item-price">
-                    ₹{{ prodPrice }}
-                </p>
-                <div v-if="prodStockQty == 0" class="out-of-stock yellow" id="red">Out of Stock</div>
+                <p class="store-item-title">{{ prodName }} ({{ prodQty }})</p>
+                <p class="store-item-price">₹{{ prodPrice }}</p>
+                <div
+                    v-if="prodStockQty == 0"
+                    class="out-of-stock yellow"
+                    id="red"
+                >
+                    Out of Stock
+                </div>
                 <div v-if="prodStockQty <= 10">
-                    <div v-if="prodStockQty > 0" class="out-of-stock yellow">Moving Fast</div>
+                    <div v-if="prodStockQty > 0" class="out-of-stock yellow">
+                        Moving Fast
+                    </div>
                 </div>
             </div>
         </div>
 
         <div class="quantity">
-            <button class="minus">
+            <button class="minus" @click="removeFromCart()">
                 <div>-</div>
             </button>
-            <div class="qty">0</div>
-            <button class="plus">
+
+            <div class="qty">{{ count }}</div>
+
+            <button class="plus" @click="addToCart()">
                 <div class="white">+</div>
             </button>
         </div>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -8,68 +8,59 @@ import Product from "../components/Product.vue";
 import Footer from "../components/Footer.vue";
 </script>
 
-<template>
-    <div class="page">
-        <h1 class="title txt-ctr">Cart</h1>
-
-        <div class="store">
-            <div v-for="(item, index) in shopItems" :key="index">
-                <Product
-                    :prodName= "item.prodName"
-                    :prodImage= "item.prodImage"
-                    :prodQty= "item.prodQty"
-                    :prodPrice= "item.prodPrice"
-                    :prodStockQty= "item.prodStockQty"
-                ></Product>
-            </div>
-            <Product prodName="Potatos" prodImage="tomatos.png" prodQty="1kg" prodPrice=29 prodStockQty=50 />
-        </div>
-
-        <RouterLink to="/checkout">
-            <div class="black-btn">
-                CHECKOUT
-            </div>
-        </RouterLink>
-    </div>
-    <Footer />
-</template>
-
 <script>
 export default {
+    data() {
+        return {
+            categories: [],
+            cart: {},
+        };
+    },
+    methods: {
+        async getData() {
+            console.log("First time render");
+            try {
+                const response = await fetch("http://127.0.0.1:5000/cart");
+                const data = await response.json();
+                this.cart = data.cart;
+                console.log(this.items);
+            } catch (err) {
+                console.log(err.message);
+            }
+        },
+    },
+    created() {
+        this.getData();
+    },
     components: {
         Product,
         Footer,
     },
-    data() {
-        return {
-            shopItems: [
-                {
-                    prodName: "Potatos",
-                    prodImage: "tomatos.png",
-                    prodQty: "1kg",
-                    prodPrice: 29,
-                    prodStockQty: 50,
-                }, {
-                    prodName: "Potatos",
-                    prodImage: "tomatos.png",
-                    prodQty: "1kg",
-                    prodPrice: 29,
-                    prodStockQty: 50,
-                }, {
-                    prodName: "Potatos",
-                    prodImage: "tomatos.png",
-                    prodQty: "1kg",
-                    prodPrice: 29,
-                    prodStockQty: 50,
-                }, {
-                    prodName: "Potatos",
-                    prodImage: "tomatos.png",
-                    prodQty: "1kg",
-                    prodPrice: 29,
-                    prodStockQty: 50,
-                }
-            ],
-        }
-    }
-}
+};
 </script>
+
+<template>
+    <div class="page">
+        <h1 class="title txt-ctr">Cart</h1>
+
+        <div class="store small-page">
+            <div v-for="prod in cart" :key="prod[0]">
+                <Product
+                    :prodId="prod[0]"
+                    :prodName="prod[1]"
+                    :prodImage="prod[5]"
+                    :prodQty="prod[2]"
+                    :prodPrice="prod[3]"
+                    :prodStockQty="prod[7]"
+                    :count="prod[8]"
+                    :screen="cart"
+                ></Product>
+            </div>
+        </div>
+
+        <RouterLink to="/checkout">
+            <div class="black-btn">CHECKOUT</div>
+        </RouterLink>
+    </div>
+    <Footer />
+</template>
