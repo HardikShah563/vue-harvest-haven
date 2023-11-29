4d729f1fb68d856544dfb62a8b0442b0e7351950
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2023-11-29 21:20:28 +0530

    delete item works

--- src/views/DelItem.vue
+++ src/views/DelItem.vue
@@ -1,47 +1,110 @@
-<script>
+<script setup>
+// importing stylesheets
 import "../assets/form.css";
+// importing components
+import Footer from "../components/Footer.vue";
+</script>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "",
+            msgColor: "",
+            items: "",
+            response: {},
+            formData: {
+                p_id: "",
+            },
+        };
+    },
+    methods: {
+        async getData() {
+            console.log("First time render");
+            try {
+                const response = await fetch(
+                    "http://127.0.0.1:5000/delete-item"
+                );
+                const data = await response.json();
+                this.items = data.items;
+            } catch (err) {
+                console.log(err.message);
+            }
+        },
+
+        async deleteItem() {
+            try {
+                await fetch("http://127.0.0.1:5000/delete-item", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(this.formData),
+                })
+                    .then((response) => response.json())
+                    .then(async (data) => {
+                        if (data.message) {
+                            this.msg = data.message;
+                            this.msgColor = "green";
+                        } else {
+                            this.msg = data.error;
+                            this.msgColor = "red";
+                        }
+                    });
+            } catch (err) {
+                console.log(err.message);
+            }
+        },
+    },
+    created() {
+        this.getData();
+    },
+    components: {
+        Footer,
+    },
+};
 </script>
 
 <template>
     <div className="admin-forms page">
-                <div className="form">
-                    <h1 className="title">Delete Product</h1>
-                    <h1 className="subtitle">** Note: The action cannot be revoked, think before deleting **</h1>
+        <div className="form">
+            <h1 className="title">Delete Product</h1>
+            <h1 className="subtitle">
+                ** Note: The action cannot be revoked, think before deleting **
+            </h1>
 
-                    <form>
-                        <div className="input-box">
-                            <div className="input msg" id={color}>
-                                {msg}
-                            </div>
-                        </div>
-
-                        <div className="input-box">
-                            <label
-                                htmlFor="p_id">
-                                Select Item to be deleted
-                            </label>
-
-                            <select
-                                className="input"
-                                name="p_id"
-                                id="p_id"
-                                value={formData.p_id}
-                                required>
-                                <option value="">-- None --</option>
-                                {products.map((prod) => (
-                                    <option value={prod._id}>{prod.name}</option>
-                                ))}
-                            </select>
-                        </div>
-
-                        <div className="input-box">
-                            <button
-                                className="form-btn"
-                                type="submit">
-                                Delete Product
-                            </button>
-                        </div>
-                    </form>
+            <form @submit.prevent="deleteItem()">
+                <div className="input-box">
+                    <div className="input msg" :id="msgColor">{{ msg }}</div>
                 </div>
-            </div>
+
+                <div className="input-box">
+                    <label htmlFor="p_id"> Select Item to be deleted </label>
+
+                    <select
+                        className="input"
+                        name="p_id"
+                        id="p_id"
+                        v-model="formData.p_id"
+                        required
+                    >
+                        <option value="">-- None --</option>
+                        <option
+                            v-for="item in items"
+                            :key="item[0]"
+                            :value="item[0]"
+                        >
+                            {{ item[1] }}
+                        </option>
+                    </select>
+                </div>
+
+                <div className="input-box">
+                    <button className="form-btn" type="submit">
+                        Delete Product
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
